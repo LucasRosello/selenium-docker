633bd0952e21e89d3600d0407fb87d512c226ac2
Lucas Rosello <l.martin.rosello@gmail.com>
2022-08-19 14:28:04 +0000

    start codding the bot

---
+++ readme.md
@@ -0,0 +1,3 @@
+docker run -d -p 4444:4444 --shm-size="2g" selenium/standalone-chrome:4.4.0-20220812
+
+node test.js
--- test.js
+++ test.js
@@ -4,30 +4,34 @@ const { Builder, By, Key, until } = require("selenium-webdriver");
 (async function googleSearch() {
   let driver = await new Builder()
     .forBrowser("chrome")
-    // .usingServer("http://localhost:4444/wd/hub/")
-    .setChromeService(
-      chrome.setDefaultService(
-        new chrome.ServiceBuilder("./drivers/chromedriver_89").build()
-      )
-    )
+    .usingServer("http://localhost:4444/wd/hub/")
+    // .setChromeService(
+    //   chrome.setDefaultService(
+    //     new chrome.ServiceBuilder("./drivers/chromedriver_89").build()
+    //   )
+    // )
     .build();
 
   try {
     // Navigate to Url
-    await driver.get("https://www.google.com");
-    // Enter text "Automation Bro" and perform keyboard action "Enter"
-    await driver
-      .findElement(By.name("q"))
-      .sendKeys("Automation Bro", Key.ENTER);
+    console.log("corriendo")
+    await driver.get("https://allaria-ssl.allaria.com.ar/AllariaOnline/VBolsaNet/login.html");
+    await driver.wait(until.titleIs('ALLARIA LEDESMA & CIA. S.A'), 1000);
+    console.log(await driver.getTitle())
 
-    let firstResult = await driver.wait(
-      until.elementLocated(By.css("h3")),
-      10000
-    );
+    let userInput =
+       await driver.wait(until.elementLocated(By.id('input_0')), 10000);
 
-    console.log(await firstResult.getAttribute("textContent"));
-    console.log(await (await driver.getCapabilities()).getBrowserName());
-    console.log(await (await driver.getCapabilities()).getBrowserVersion());
+       await userInput.sendKeys("hola")
+
+       driver.await(50000, TimeUnit.Milliseconds)
+    /*
+      let button =
+       await driver.wait(until.elementLocated(By.id('foo')), 10000);
+   await button.click();
+*/
+
+    
   } finally {
     driver.quit();
   }
