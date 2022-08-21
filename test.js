fb0ae246b8f70e6032fd815941a7b89467e550c6
Lucas Rosello <l.martin.rosello@gmail.com>
2022-08-21 03:51:05 +0000

    go to status page

--- test.js
+++ test.js
@@ -19,9 +19,8 @@ const { Builder, By, Key, until } = require("selenium-webdriver");
     await driver.wait(until.titleIs('ALLARIA LEDESMA & CIA. S.A'), 1000);
     console.log(await driver.getTitle())
 
-    let userInput =
-       await driver.wait(until.elementLocated(By.id('input_0')), 10000);
-       await userInput.sendKeys(process.env.WINA_USER)
+    let userInput = await driver.wait(until.elementLocated(By.id('input_0')), 10000);
+    await userInput.sendKeys(process.env.WINA_USER)
 
     let passInput = await driver.wait(until.elementLocated(By.id('input_1')), 10000);
     await passInput.sendKeys(process.env.WINA_PASS)
@@ -29,8 +28,15 @@ const { Builder, By, Key, until } = require("selenium-webdriver");
     let loginButton = await driver.wait(until.elementLocated(By.id('btnIngresar')), 10000);
     await loginButton.click()
 
+    await driver.wait(until.elementLocated(By.xpath('/html/body/div[3]/md-content/md-content/div[1]/ng-view/div/div[2]/div/div/div/div/div/ng-transclude/div[1]/span[1]/div')), 20000);
 
-       driver.await(50000, TimeUnit.Milliseconds)
+    await driver.get("https://allaria-ssl.allaria.com.ar/AllariaOnline/VBolsaNet/desktop.html#!/tenenciaval");
+
+    // fake id for waiting
+    await driver.wait(until.elementLocated(By.id('fakeid')), 10000);
+
+    //pnlTV_container
+       
     /*
       let button =
        await driver.wait(until.elementLocated(By.id('foo')), 10000);
